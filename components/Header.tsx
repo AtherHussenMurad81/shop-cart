1b588f13762a4c509e6fa962bf2583fce1f0bf87
Ather Hussen Murad <muradnowshad20@gmail.com>
2026-03-22 16:26:27 +0600

    wishlish and favorite added

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,16 +2,23 @@ import React from "react";
 import Container from "./Container";
 import Logo from "./Logo";
 import HeaderMenu from "./HeaderMenu";
+import SearchBar from "./SearchBar";
+import CartIcon from "./CartIcon";
+import FavoriteButton from "./FavoriteButton";
 
 const Header = () => {
   return (
-    <header className="bg-white py-5 border-b border-b-black">
+    <header className="bg-white py-5 border-b border-b-black/20">
       <Container className="flex items-center justify-between">
         {/* Logo */}
         <Logo></Logo>
         {/* navbtn */}
         <HeaderMenu></HeaderMenu>
-        <div>Other</div>
+        <div className="flex w-auto md:1/3 items-center justify-end gap-5">
+          <SearchBar />
+          <CartIcon />
+          <FavoriteButton />
+        </div>
         {/* navadmin */}
       </Container>
     </header>
